4113a77b1046029959bab54b9957081f36cb182a
Adam Feehely <adamfeehely@gmail.com>
2024-11-25 11:59:42 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QP0gaRu83DzBahp6PRsX49VPQWnfPZsVHbeKrunVWtFaMigbIRotMNi30pLbajfE7fSay91orDywi4e4WOMHrhF00QHuzSwDq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
